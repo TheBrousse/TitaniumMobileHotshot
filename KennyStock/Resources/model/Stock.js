599f86af35e37dcd379d8b363ab2d1478ffcb124
Christian Brousseau <cbrousseau@hotmail.com>
2013-01-05 23:24:04 +0100

    Fihished Settings L&F

--- KennyStock/Resources/model/Stock.js
+++ KennyStock/Resources/model/Stock.js
@@ -1,7 +1,7 @@
 function Stock(symbol, quantity) {
     this.symbol = symbol;
-    this.quantity = quantity;
     this.price = 0.00;
+    this.quantity = quantity;
 }
 
 module.exports = Stock;
--- KennyStock/Resources/ui/common/SettingsView.js
+++ KennyStock/Resources/ui/common/SettingsView.js
@@ -6,25 +6,30 @@ function SettingsView() {
     
 	//create object instance, a parasitic subclass of Observable
 	var self = Ti.UI.createView({
-		backgroundColor: 'blue',
+		backgroundColor: '#006600',
 		width: '100%',
 		height: '100%'
 	});
 	
 	self.add(Ti.UI.createLabel({
-	    top: 15,
+	    left: 10,
+	    top: 30,
 	    width: Ti.UI.SIZE,
 	    height: Ti.UI.SIZE,
-	    text: 'Your money objective:'
+	    text: 'Your money objective ($):',
+    	font: {
+    	    fontSize: '14sp',
+            fontWeight: 'bold'
+        }
 	}));
 	
 	var txtObjective = Ti.UI.createTextField({
-	    left: 10,
-        top: 45,
-        width: '90%',
+	    right: 15,
+        top: 15,
+        width: 190,
         height: Ti.UI.SIZE,
         backgroundColor: '#ffffff',
-        hintText: 'Enter Amount',
+        hintText: 'Amount',
         keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD
 	});
 
@@ -32,16 +37,20 @@ function SettingsView() {
 
     self.add(Ti.UI.createLabel({
         left: 10,
-        top: 75,
+        top: 100,
         width: Ti.UI.SIZE,
         height: Ti.UI.SIZE,
-        text: 'Symbol'
+        text: 'Symbol:',
+        font: {
+            fontSize: '14sp',
+            fontWeight: 'bold'
+        }
     }));
     
     var txtSymbol = Ti.UI.createTextField({
-        left: 10,
-        top: 75,
-        width: 30,
+        left: 97,
+        top: 85,
+        width: 100,
         height: Ti.UI.SIZE,
         backgroundColor: '#ffffff'
     });
@@ -49,38 +58,46 @@ function SettingsView() {
     self.add(txtSymbol);
     
     self.add(Ti.UI.createLabel({
-        left: 100,
-        top: 75,
+        left: 200,
+        top: 100,
         width: Ti.UI.SIZE,
         height: Ti.UI.SIZE,
-        text: 'Quantity'
+        text: 'Quantity:',
+        font: {
+            fontSize: '14sp',
+            fontWeight: 'bold'
+        }
     }));
 
     var txtQuantity = Ti.UI.createTextField({
-        left: 175,
-        top: 75,
-        width: 30,
+        left: 310,
+        top: 85,
+        width: 75,
         height: Ti.UI.SIZE,
         keyboardType: Ti.UI.KEYBOARD_NUMBER_PAD,
-        backgroundColor: '#ffffff',
+        backgroundColor: '#ffffff'
     });
     
     self.add(txtQuantity);
     
     var btnAddStock = Ti.UI.createButton({
-        left: 200,
-        top: 75,
+        right: 15,
+        top: 85,
+        width: 75,
         title: 'Add'
     });
     
     self.add(btnAddStock);
     
-   var stockList = Ti.UI.createTableView({
+    var stockList = Ti.UI.createTableView({
         left: 0,
-        top: 120,
+        top: 170,
         width: Ti.UI.FILL,
-        height: '65%',
-        data: [{ title: 'item1' }]
+        height: '60%',
+        data: [{ title: 'item1' },
+            { title: 'item2' },
+            { title: 'item17' }
+       ]
     }); 
     
     self.add(stockList);
@@ -95,16 +112,62 @@ function SettingsView() {
 	btnAddStock.addEventListener('click', function() {
         var stock = new Stock(txtSymbol.value.toUpperCase(), txtQuantity.value);
         
+        Ti.API.info(JSON.stringify(stock));
+        
+        addRow(stockList, stock);
     });
 	
 	btnSave.addEventListener('click', function() {
-	    ps.saveObjective(txtObjective.value);
-	    ps.saveQuotes(quotes);	    
+	    ps.saveObjective(parseInt(txtObjective.value) || 0);
+	    //ps.saveQuotes(quotes);	    
 	    
 	    self.fireEvent('settings:close');
 	});
 	
+	var stock = new Stock('IBM', 55);
+        
+    addRow(stockList, stock);
+    addRow(stockList, stock);
+    addRow(stockList, stock);
+	
+	
 	return self;
+}
+
+function addRow(table, stock) {
+    var row = Ti.UI.createTableViewRow();
+    
+    row.add(Ti.UI.createLabel({
+        text: stock.symbol,
+        left: 5, 
+        top: 1,
+        font: {
+            fontSize: '15sp',
+            fontWeight: 'bold'
+        }
+    }));
+    
+    row.add(Ti.UI.createLabel({
+        text: 'Latest Price: ' + stock.price + '$',
+        right: 5, 
+        top: 1,
+        font: {
+            fontSize: '9sp',
+        }
+    }));
+    
+    row.add(Ti.UI.createLabel({
+        text: 'x' + stock.quantity,
+        right: 5, 
+        top: 20,
+        font: {
+            fontSize: '8sp',
+            fontStyle: 'italic'
+        }
+    }));
+    
+    
+    table.appendRow(row);
 }
 
 module.exports = SettingsView;
