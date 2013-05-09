67c2bccaab87dd66a3089a4abee96b48bc4cbdf6
Christian Brousseau <cbrousseau@hotmail.com>
2013-05-10 01:33:07 +0200

    Updates

--- SecondToLastFantasyOnline/Resources/app.js
+++ SecondToLastFantasyOnline/Resources/app.js
@@ -44,14 +44,14 @@ else if ('socket.io' === test) {
 	var io = require('socket.io'),
 	socket = io.connect(uri);
 	
-    // socket.on('connecting', function () { Ti.API.log('connecting'); });
+    socket.on('connecting', function () { Ti.API.log('connecting'); });
     socket.on('connect', function () { Ti.API.log('connected'); });
-    // socket.on('connect_failed', function (e) { Ti.API.log('connect_failed' + JSON.stringify(e)); });
-    // socket.on('error', function (e) { Ti.API.log('error: ' + JSON.stringify(e)); });
-    // socket.on('reconnecting', function (num) { Ti.API.log('reconnecting attempt #' + num); });
-    // socket.on('reconnect', function () { Ti.API.log('reconnected'); });
-    // socket.on('reconnect_failed', function (e) { Ti.API.log('reconnect_failed' + JSON.stringify(e)); });
-	// socket.on('disconnect', function() { Ti.API.log('disconnected'); });
+    socket.on('connect_failed', function (e) { Ti.API.log('connect_failed' + JSON.stringify(e)); });
+    socket.on('error', function (e) { Ti.API.log('error: ' + JSON.stringify(e)); });
+    socket.on('reconnecting', function (num) { Ti.API.log('reconnecting attempt #' + num); });
+    socket.on('reconnect', function () { Ti.API.log('reconnected'); });
+    socket.on('reconnect_failed', function (e) { Ti.API.log('reconnect_failed' + JSON.stringify(e)); });
+	socket.on('disconnect', function() { Ti.API.log('disconnected'); });
     
 	socket.on('message', function(msg) {
 	   Ti.API.info(msg); 
@@ -88,3 +88,26 @@ else if ('nowjs' === test) {
 		return 'ille qui nos omnes servabit';
 	};
 }
+
+var win = Ti.UI.createWindow({
+    backgroundColor: '#000'
+});
+
+var txtMsg = Ti.UI.createTextField({
+    width: '95%',
+    top: 10 
+});
+
+var btnSend = Ti.UI.createButton({
+    title: 'Send',
+    top: 85
+});
+
+btnSend.addEventListener('click', function() {
+    socket.send(txtMsg.value);
+});
+
+win.add(txtMsg);
+win.add(btnSend);
+
+win.open();
