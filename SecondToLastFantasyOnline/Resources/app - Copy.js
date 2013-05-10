1c772c1b5e039f2d379e2e7f06377fb26e82c72d
Christian Brousseau <cbrousseau@hotmail.com>
2013-05-10 20:37:51 +0200

    Added features to the server code

--- SecondToLastFantasyOnline/Resources/app - Copy.js
+++ SecondToLastFantasyOnline/Resources/app - Copy.js
@@ -1,46 +1,5 @@
-// ****************************************************************************************************************
-// ****************************************************************************************************************
-// test value can be 'raw' | 'socket.io' | 'nowjs'
-
-var test = 'socket.io', 
-
-// ****************************************************************************************************************
-// ****************************************************************************************************************
-// REMEMBER to change this with your data
-
 uri = 'ws://192.168.1.10:8080'; 
 
-// ****************************************************************************************************************
-// ****************************************************************************************************************
-// example using a plain websockets
-
-if ('raw' === test) {
-	var WS = require('net.iamyellow.tiws').createWS();
-
-	WS.addEventListener('open', function () {
-		Ti.API.debug('websocket opened');
-	});
-
-	WS.addEventListener('close', function (ev) {
-		Ti.API.info(ev);
-	});
-
-	WS.addEventListener('error', function (ev) {
-		Ti.API.error(ev);
-	});
-
-	WS.addEventListener('message', function (ev) {
-		Ti.API.log(ev);
-	});
-	
-	WS.open(uri);
-}
-
-// ****************************************************************************************************************
-// ****************************************************************************************************************
-// example using socket.io which uses websocket
-
-else if ('socket.io' === test) {
 	var io = require('socket.io'),
 	socket = io.connect(uri);
 	
@@ -56,38 +15,7 @@ else if ('socket.io' === test) {
 	socket.on('message', function(msg) {
 	   Ti.API.info(msg); 
 	});
-}
 
-// ****************************************************************************************************************
-// ****************************************************************************************************************
-// example using now.js which uses socket.io which uses websockets
-
-else if ('nowjs' === test) {
-	var now = require('now').nowInitialize(uri, {
-		// socket.io init options
-		socketio: {
-			transports: ['websocket']
-		}
-	});
-
-	now.core.on('error', function () {
-		Ti.API.error('error!')
-	});
-
-	now.core.on('ready', function () {
-		Ti.API.log('now is now ready')
-	});
-
-	now.core.on('disconnect', function () {
-		Ti.API.log('now disconnected');
-	});
-
-	// now data bindings
-	now.userID = '4815162342';
-	now.whatLiesInTheShadowOfTheStatue = function () {
-		return 'ille qui nos omnes servabit';
-	};
-}
 
 var win = Ti.UI.createWindow({
     backgroundColor: '#000'
--- SecondToLastFantasyOnlineServer/server/app.js
+++ SecondToLastFantasyOnlineServer/server/app.js
@@ -13,10 +13,42 @@ server.listen(8080, function() {
 	console.log('Listening at: http://localhost:8080');
 });
  
+var players = [];
+
 socketio.listen(server).on('connection', function (socket) {
-	socket.on('message', function (msg) {
-		console.log('Message Received: ', msg);
-		socket.broadcast.emit('message', msg);
+
+	socket.on('join', function (player) {
+		console.log('Player ', player.id, ' has joined');
+
+		users.push(player);
+
+		for (u in users) {
+			socket.broadcast('newplayer', users[u]);
+		}
 	});
+
+	socket.on('quit', function (player) {
+		users.remove(player.id);
+
+		for (u in users) {
+			if (users[u].id === player.id) {
+				console.log('Player ', player.id, ' has quit');
+
+				socket.broadcast('quit', users[u]);
+			}
+		}
+	});
+
+	socket.on('speak', function (player) {
+		console.log('Player ', player.id, ' said: ', player.caption);
+
+		socket.broadcast.emit('message', player.caption);
+	});
+
+	socket.on('move', function (player) {
+		console.log('Player ', player.id, ' moved to x: %d - y: %d', player.x, player.y);
+
+		socket.broadcast.emit('move', player);
+	})
 });
 
