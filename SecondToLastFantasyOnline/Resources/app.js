56796be381b45f3dbb3c2a3ab29b0653988f21e4
Christian Brousseau <cbrousseau@hotmail.com>
2013-05-22 20:55:51 +0200

    Implemented every missing event

--- SecondToLastFantasyOnline/Resources/app.js
+++ SecondToLastFantasyOnline/Resources/app.js
@@ -4,7 +4,7 @@ var win = Ti.UI.createWindow({ backgroundColor: 'black' });
 var quicktigame2d = require('com.googlecode.quicktigame2d');
 var io = require('socket.io'),
 
-SERVER_URI = 'ws://9.143.237.89:8080';
+SERVER_URI = 'ws://192.168.1.18:8080';
 
 // Connect to the Game Server
 socket = io.connect(SERVER_URI);
@@ -19,24 +19,52 @@ socket.on('reconnect', function () { Ti.API.log('reconnected'); });
 socket.on('reconnect_failed', function (e) { Ti.API.log('reconnect_failed' + JSON.stringify(e)); });
 socket.on('disconnect', function() { Ti.API.log('disconnected'); });
 
+var players = [];
 
 socket.on('playerjoined', function(player) {
- //   $('#activePlayers').append($('<li></li>').text(player.id));
-    Ti.API.info('player ' + player.id + ' joined the game');
+	Ti.API.info('player ' + JSON.stringify(player) + ' joined the game');
+	var newPlayer = new Character(scene, player.imageSheet);
+	newPlayer.id = player.id;
+	newPlayer.z = 2;
+
+	players.push(newPlayer);
 });
 
 socket.on('playersaid', function(player) {
-    Ti.API.info(player.id + ': ' + player.caption);
+	Ti.API.info(player.id + ': ' + player.caption);
+
+	var p = getPlayer(player.id);
+			
+	p.say(player.caption);
 });
 
 socket.on('playermoved', function(player) {
-    Ti.API.trace('player ' + player.id + ' moved x: ' + player.x + '  y: ' + player.y);
+	Ti.API.trace('player ' + player.id + ' moved x: ' + player.x + '  y: ' + player.y);
+
+	var p = getPlayer(player.id);
+	
+	p.x = player.x;
+	p.y = player.y;
 });
 
 socket.on('playerquit', function(player) {
-  //  $.find('#activePlayers li:contains(' + player.id + ')')[0].remove()
-    Ti.API.info('player ' + player.id + ' quit the game');
+	Ti.API.info('player ' + player.id + ' quit the game');
+
+	for (p in players) {
+		if (players[p].id === player.id) {
+			scene.remove(players[p]);
+			players.splice(p);
+		}
+	}
 });
+
+function getPlayer(id) {
+	for (p in players) {
+		if (players[p].id === id) {
+			return players[p];
+		}
+	}
+}
 
 
 // Create view for your game.
@@ -79,11 +107,11 @@ var ground_layer = mapjson.layers[0];
 var items_layer = mapjson.layers[1];
 
 var mapinfo = {
-    image: 'assets/' + desert_tileset.image,
-    tileWidth: desert_tileset.tilewidth,
-    tileHeight: desert_tileset.tileheight,
-    border: desert_tileset.spacing,
-    margin: desert_tileset.margin
+	image: 'assets/' + desert_tileset.image,
+	tileWidth: desert_tileset.tilewidth,
+	tileHeight: desert_tileset.tileheight,
+	border: desert_tileset.spacing,
+	margin: desert_tileset.margin
 };
 
 // create ground map layer
@@ -135,133 +163,155 @@ var heroDirection = "DOWN";
 // Onload event is called when the game is loaded.
 // The game.screen.width and game.screen.height are not yet set until this onload event.
 game.addEventListener('onload', function(e) {
-    // set screen size for your game (non-retina size)
-    var screenScale = game.size.width / Ti.Platform.displayCaps.platformWidth;
+	// set screen size for your game (non-retina size)
+	var screenScale = game.size.width / Ti.Platform.displayCaps.platformWidth;
 
-    game.screen = {
-        width: game.size.width / screenScale,
-        height: game.size.height / screenScale
-    };
+	game.screen = {
+		width: game.size.width / screenScale,
+		height: game.size.height / screenScale
+	};
 
-    // Your game screen size is set here if you did not specifiy game width and height using screen property.
-    // Note: game.size.width and height may be changed due to the parent layout so check them here.
-    Ti.API.info("view size: " + game.size.width + "x" + game.size.height);
-    Ti.API.info("game screen size: " + game.screen.width + "x" + game.screen.height);
-    Ti.API.info("map: " + map.width + "x" + map.height);
+	// Your game screen size is set here if you did not specifiy game width and height using screen property.
+	// Note: game.size.width and height may be changed due to the parent layout so check them here.
+	Ti.API.info("view size: " + game.size.width + "x" + game.size.height);
+	Ti.API.info("game screen size: " + game.screen.width + "x" + game.screen.height);
+	Ti.API.info("map: " + map.width + "x" + map.height);
 
-    WINDOW_SCALE_FACTOR_X = game.screen.width  / game.size.width;
-    WINDOW_SCALE_FACTOR_Y = game.screen.height / game.size.height;
+	WINDOW_SCALE_FACTOR_X = game.screen.width  / game.size.width;
+	WINDOW_SCALE_FACTOR_Y = game.screen.height / game.size.height;
 
-    vpad.x = (game.screen.width * 0.5) - (vpad.width * 0.5);
-    vpad.y = game.screen.height - vpad.height;
+	vpad.x = (game.screen.width * 0.5) - (vpad.width * 0.5);
+	vpad.y = game.screen.height - vpad.height;
 
-    chat_button.y = game.screen.height - chat_button.height;
+	chat_button.y = game.screen.height - chat_button.height;
 
-    centerHero();
+	centerHero();
 
-    // Join the game
-    hero.id = Ti.Platform.id;
-    socket.emit('join', hero);
+	// Start the game
+	game.start();
 
-    // Start the game
-    game.start();
-
-    updateVpadTimerID = setInterval(function(e) {
-        updateVpad();
-    }, 66);
+	updateVpadTimerID = setInterval(function(e) {
+		updateVpad();
+	}, 66);
 
 });
 
 /// Stop update timer before app is closed
 win.addEventListener('android:back', function(e) {
-    clearInterval(updateVpadTimerID);
+	clearInterval(updateVpadTimerID);
 
-    win.close();
+	win.close();
 });
 
 function updateVpad() {
-    if (isVpadActive) {
-        // Is the character moving fast or slow?
-        var powerX = (touchX - (vpad.x + (vpad.width  * 0.5))) * 0.2;
-        var powerY = (touchY - (vpad.y + (vpad.height * 0.5))) * 0.2;
+	if (isVpadActive) {
+		// Is the character moving fast or slow?
+		var powerX = (touchX - (vpad.x + (vpad.width  * 0.5))) * 0.2;
+		var powerY = (touchY - (vpad.y + (vpad.height * 0.5))) * 0.2;
 
-        vpad.color(0.78, 0.78, 0.78);
-        vpad_nav.x = touchX - (vpad_nav.width  * 0.5);
-        vpad_nav.y = touchY - (vpad_nav.height * 0.5);
-        vpad_nav.show();
+		vpad.color(0.78, 0.78, 0.78);
+		vpad_nav.x = touchX - (vpad_nav.width  * 0.5);
+		vpad_nav.y = touchY - (vpad_nav.height * 0.5);
+		vpad_nav.show();
 
-        // Change animation of the hero's sprite
-        // Is the wanted direction more vertical or horizontal
-        if (Math.abs(powerX) > Math.abs(powerY)) { // Horizontal
-            heroDirection = (powerX < 0) ? "LEFT" : "RIGHT";
-        } else { // Vertical
-            heroDirection = (powerY < 0) ? "UP" : "DOWN";
-        }
+		// Change animation of the hero's sprite
+		// Is the wanted direction more vertical or horizontal
+		if (Math.abs(powerX) > Math.abs(powerY)) { // Horizontal
+			heroDirection = (powerX < 0) ? "LEFT" : "RIGHT";
+		} else { // Vertical
+			heroDirection = (powerY < 0) ? "UP" : "DOWN";
+		}
 
-        hero.turnTowards(heroDirection);
+		hero.turnTowards(heroDirection);
 
-        var nextHeroX = hero.x + powerX;
-        var nextHeroY = hero.y + powerY;
+		var nextHeroX = hero.x + powerX;
+		var nextHeroY = hero.y + powerY;
 
-        var nextMapX = map.x - powerX;
-        var nextMapY = map.y - powerY;
+		var nextMapX = map.x - powerX;
+		var nextMapY = map.y - powerY;
 
-        // move hero and map layers
-        if (nextHeroX > 0 && nextHeroX < (game.screen.width  - hero.width)) {
-            hero.x = nextHeroX;
-        } else if (nextMapX <= 0 && nextMapX > (-map.width + game.screen.width)) {
-            map.x = nextMapX;
-            map_items.x = map.x;
-        }
-        if (nextHeroY > 0 && nextHeroY < (game.screen.height - hero.height)) {
-            hero.y = nextHeroY;
-        } else if (nextMapY <= 0 && nextMapY > (-map.height + game.screen.height)) {
-            map.y = nextMapY;
-            map_items.y = map.y;
-        }
+		// move hero and map layers
+		if (nextHeroX > 0 && nextHeroX < (game.screen.width  - hero.width)) {
+			hero.x = nextHeroX;
+		} else if (nextMapX <= 0 && nextMapX > (-map.width + game.screen.width)) {
+			map.x = nextMapX;
+			map_items.x = map.x;
+		}
+		if (nextHeroY > 0 && nextHeroY < (game.screen.height - hero.height)) {
+			hero.y = nextHeroY;
+		} else if (nextMapY <= 0 && nextMapY > (-map.height + game.screen.height)) {
+			map.y = nextMapY;
+			map_items.y = map.y;
+		}
 
-        socket.emit('move', hero);
-        Ti.API.debug('x: '+hero.x + '  y: '+ hero.y + ' | map-x: '+map.x + '  map-y: '+ map.y );
-    } else {
-        vpad.color(1, 1, 1);
-        vpad_nav.hide();
+		drawOtherPlayers()
 
-        hero.halt();
-    }
+		// Send absolute coordinated to the server
+		socket.emit('move', {
+			id: hero.id,
+			x: Math.round(Math.abs(hero.x + map.x)),
+			y: Math.round(Math.abs(hero.y + map.y))
+		});
+		Ti.API.debug('x: '+hero.x + '  y: '+ hero.y + ' | map-x: '+map.x + '  map-y: '+ map.y );
+	} else {
+		vpad.color(1, 1, 1);
+		vpad_nav.hide();
+
+		hero.halt();
+	}
+}
+
+function drawOtherPlayers() {
+	//Il faut maintenant que tu testes si cet autre joueur est effectivement visible dans le VP. Il faut que toutes les conditions suivantes soient vraies :
+	// le bord droit du sprite est à droite du bord gauche de l'écran
+	for (p in players) {
+		var otherHero = players[p];
+
+		if (((otherHero.x + hero.width/2) >= 0) &&
+			// le bord gauche du sprite est à gauche du bord droit de l'écran
+			 ((otherHero.x - hero.width/2) <= game.screen.width) &&
+			// le bord bas du sprite est en-dessous du bord haut de l'écran
+			((otherHero.y + hero.height/2) >= 0) &&
+			// le bord haut du sprite est au-dessus du bord bas de l'écran
+			((otherHero.y - hero.height/2) <= game.screen.height)) {
+				Ti.API.info('all conditions are true');
+		}
+
+	}
 }
 
 function centerHero() {
-    hero.x = (game.screen.width * 0.5) - (hero.width * 0.5);
-    hero.y = (game.screen.height * 0.5) - (hero.height * 0.5);
+	hero.x = (game.screen.width * 0.5) - (hero.width * 0.5);
+	hero.y = (game.screen.height * 0.5) - (hero.height * 0.5);
 }
 
 var ChatView = require('chat');
 var chatView = new ChatView(function(caption) {
-    hero.say(caption);
+	hero.say(caption);
 });
 
 win.add(chatView);
 
 game.addEventListener('touchstart', function(e) {
-    touchX = (e.x * WINDOW_SCALE_FACTOR_X);
-    touchY = (e.y * WINDOW_SCALE_FACTOR_Y);
+	touchX = (e.x * WINDOW_SCALE_FACTOR_X);
+	touchY = (e.y * WINDOW_SCALE_FACTOR_Y);
 
-    isVpadActive = vpad.contains(touchX, touchY);
+	isVpadActive = vpad.contains(touchX, touchY);
 
-    if (chat_button.contains(touchX, touchY)) {
-        chatView.show();
-    }
+	if (chat_button.contains(touchX, touchY)) {
+		chatView.show();
+	}
 });
 
 game.addEventListener('touchmove', function(e) {
-    touchX = (e.x * WINDOW_SCALE_FACTOR_X);
-    touchY = (e.y * WINDOW_SCALE_FACTOR_Y);
+	touchX = (e.x * WINDOW_SCALE_FACTOR_X);
+	touchY = (e.y * WINDOW_SCALE_FACTOR_Y);
 
-    isVpadActive = vpad.contains(touchX, touchY);
+	isVpadActive = vpad.contains(touchX, touchY);
 });
 
 game.addEventListener('touchend', function(e) {
-    isVpadActive = false;
+	isVpadActive = false;
 });
 
 // load debug functions
@@ -273,11 +323,15 @@ win.add(game);
 var HeroSelectionView = require('hero_select');
 
 win.add(new HeroSelectionView(function(imageSheet) {
-    scene.remove(hero);
+	scene.remove(hero);
 
-    hero = new Character(scene, imageSheet);
-    centerHero();
-    hero.z  = 2;
+	hero = new Character(scene, imageSheet);
+	centerHero();
+	hero.z  = 2;
+
+	 // Join the game
+	hero.id = Ti.Platform.id;
+	socket.emit('join', hero);
 }));
 
 win.open({ fullscreen:true, navBarHidden:true });
