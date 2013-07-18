4a40d3589a9518f6a8e7329c620c806f0dadb8bf
Christian Brousseau <cbrousseau@hotmail.com>
2013-07-18 03:06:32 +0200

    Wrapped Window load into self containing function

--- PhotoSurrounder/Resources/app.js
+++ PhotoSurrounder/Resources/app.js
@@ -1,4 +1,11 @@
-var ApplicationWindow = require('ApplicationWindow');
-var win = new ApplicationWindow();
+//bootstrap and check dependencies
+if (Ti.version < 1.8 ) {
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');	  	
+}
 
-win.open();
+(function() {
+	var ApplicationWindow = require('ApplicationWindow');
+	var win = new ApplicationWindow();
+
+	win.open();
+})();
